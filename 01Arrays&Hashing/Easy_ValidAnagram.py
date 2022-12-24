91bba856c17a6350311b350db3e7d96438210367
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2022-12-24 12:10:13 +0100

    Valid Anagram

---
+++ 01Arrays&Hashing/Easy_ValidAnagram.py
@@ -0,0 +1,21 @@
+# Time: O(n) and Space O(n)
+def is_anagram(s: str, t: str) -> bool:
+    if len(s) != len(t):
+        return False
+
+    countS, countT = {}, {}
+
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+
+    for c in countS:
+        if countS[c] != countT.get(c, 0):
+            return False
+
+    return True
+
+
+print(is_anagram("anagram", "nagaram"))  # True
+print(is_anagram("car", "cat"))          # False
+print(is_anagram("r", "cat"))            # False
