0ef3844e2d19d77758b86cd3ba617b031eff1fbc
Mohamad Ashamr <55653276+M0-AR@users.noreply.github.com>
2023-01-06 16:41:23 +0100

    Three sum with optimal solution

--- 02TwoPointers/Medium_3Sum.py
+++ 02TwoPointers/Medium_3Sum.py
@@ -1,4 +1,5 @@
 # Brute force solution
+# T: O(n^3)
 def three_sum_bf(nums):
     res = []
     nums_length = len(nums)
@@ -16,5 +17,34 @@ def three_sum_bf(nums):
 
 
 print(three_sum_bf([-1, 0, 1, 2, -1, -4]))  # Output: [[-1, 0, 1], [-1, -1, 2]]
-print(three_sum_bf([0, 0, 0]))              # Output: [[0, 0, 0]]
-print(three_sum_bf([-1, 0, 1]))             # [[-1, 0, 1]]
+print(three_sum_bf([0, 0, 0]))  # Output: [[0, 0, 0]]
+print(three_sum_bf([-1, 0, 1]))  # [[-1, 0, 1]]
+
+
+# T: O(n^2) S: O(1) || O(n) # depend on sort library
+def three_sum(nums):
+    res = []
+    nums.sort()
+
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:  # Move to next element if this element same as before
+            continue
+
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            three_sum = a + nums[l] + nums[r]
+            if three_sum > 0:
+                r -= 1
+            elif three_sum < 0:
+                l += 1
+            else:
+                res.append([a, nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+    return res
+
+
+print(three_sum([-1, 0, 1, 2, -1, -4]))  # Output: [[-1, 0, 1], [-1, -1, 2]]
+print(three_sum([0, 0, 0]))  # Output: [[0, 0, 0]]
+print(three_sum([-1, 0, 1]))  # Output: [[-1, 0, 1]]
